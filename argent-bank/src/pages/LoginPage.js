8b0fa549f3e0a001c2e89f41be38038df7c024e8
emmegiesse <martagiulia.scoditti@gmail.com>
2022-11-04 21:49:42 +0100

    Pages

---
+++ argent-bank/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+const LoginPage = () => {
+
+    return (
+        <div>
+            <footer className="footer">
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </footer>
+        </div>
+    );
+}
+
+export default LoginPage;

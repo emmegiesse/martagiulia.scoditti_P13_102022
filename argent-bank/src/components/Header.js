b5eacca0c32ebf4c1b367f003257f9d8e1257166
emmegiesse <martagiulia.scoditti@gmail.com>
2022-11-04 22:28:38 +0100

    header fix and style

--- argent-bank/src/components/Header.js
+++ argent-bank/src/components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'; //React
+import { useNavigate } from 'react-router-dom'; 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
-import logo from '../assets/argentBankLogo.png';
+import logo from '../assets/img/argentBankLogo.png';
 
-export default function Header() {
+const Header = () => {
     const navigate = useNavigate()
 
     const goToSignUp = () => {
@@ -27,9 +27,11 @@ export default function Header() {
                 <h1 className="sr-only">Argent Bank</h1>
             </div>
             <div className="main-nav-item" onClick = {goToSignUp}>
-            <FontAwesomeIcon className = "fa fa-user-circle " icon={faUserCircle} />
-            Sign In
+                <FontAwesomeIcon className = "fa fa-user-circle " icon={faUserCircle} />
+                <p className="main-nav-item-p"> Sign In </p>
             </div>
         </nav>
     );
 }
+
+export default Header;
